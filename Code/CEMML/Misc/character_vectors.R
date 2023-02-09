81f3b85430a63a24c6a3529820cb1ff53f3a635a
anniekellner <annie.kellner@colostate.edu>
2023-02-09 13:51:35 -0700

    Completed user settings, now just writing script in .Rmd file.
    character vector = moved into different folder

---
+++ Code/CEMML/Misc/character_vectors.R
@@ -0,0 +1,12 @@
+#####################################################
+###     VECTOR OF MODELS USED IN ANALYSES   #########
+#####################################################
+
+# This script is simply a list of models used in CEMML analyses
+# It is used with the 'steps' script to enable easy variable naming
+
+models <- c("HADGEM2-ES",
+                   "LOCA_CCSM4",
+                   "MACA_CCSM4",
+                   "MACA_CCSM4_Monthly",
+                   "MIROC6")
