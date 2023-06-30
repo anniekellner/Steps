53b5e0c556e3f33887c59427a2a7f82af5829b8a
anniekellner <annie.kellner@colostate.edu>
2023-06-30 14:33:48 -0600

    initial commit for usethis script.
    Step3 - running Dugway Proving Grounds

---
+++ Code/Misc/usethis_package_setup_git.R
@@ -0,0 +1,9 @@
+##################################################
+### CONNECT WITH GITHUB VIA USETHIS PACKAGE ######
+##################################################
+
+# The terminal is extremely slow for some reason, so usethis is an alternative for RStudio-GitHub connection
+
+library(usethis)
+
+use_git_config(user.name = "anniekellner", user.email = "annie.kellner@colostate.edu")
