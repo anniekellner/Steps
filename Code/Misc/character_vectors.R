03ffb0f023f4efa124abcf58fe6dc884bb166361
anniekellner <annie.kellner@colostate.edu>
2023-09-13 10:09:59 -0600

    Add Edwards and Malmstrom to AFB list

--- Code/Misc/character_vectors.R
+++ Code/Misc/character_vectors.R
@@ -26,4 +26,5 @@ AFB_Name <- c("Homestead_ARB",
               "South_Range_Boundary",
               "Carter_Creek_Recreation_Center_Boundary",
               "JBLE",
-              "Edwards") 
+              "Edwards_AFB",
+              "Malmstrom_AFB_Proxy") 
