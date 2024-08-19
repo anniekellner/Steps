f2207192c6853d635c773eb97c6f55f77eca2d7b
Annie Kellner <annie.kellner@colostate.edu>
2024-08-18 22:01:03 -0600

    Working on pivoting plots so data is formatted correctly for plotting

---
+++ Code/Plots/Excel_Plots.R
@@ -0,0 +1,39 @@
+#############################################################
+##########    AUTOMATE EXCEL PLOTS    #######################
+#############################################################
+
+# 8-18-2024
+# written by Annie Kellner, annie.kellner@colostate.edu
+
+# ------------  Create Directory for Plots  ----------------------- #
+
+xl_plots_dir <- paste(plots_dir,"Excel_Plots", sep = "/") # Change this folder name -- ASK TREVOR
+
+if (!dir.exists(xl_plots_dir)){
+  dir.create(xl_plots_dir)}
+
+
+# Historical Average Temperature
+
+df <- monthSum$results_baseline[1:12,]
+
+# Pivot plot so that columns names are month.abb and rows are Avg_
+# See this example (or others)
+
+billboard
+billboard %>%
+  pivot_longer(
+    cols = starts_with("wk"),
+    names_to = "week",
+    names_prefix = "wk",
+    values_to = "rank",
+    values_drop_na = TRUE
+  )
+
+df2 <- pivot_longer(data = df, cols = starts_with("Avg"), names_to = month.abb)
+
+hist_Tmean_ave <- ggplot(data = df, aes(x = month, y = Avg_TMeanF)) +
+  geom_bar(aes(color = "#CBC598", fill = "#CBC598"))
+  
+                         
+
