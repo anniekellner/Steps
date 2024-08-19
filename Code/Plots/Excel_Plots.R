f90c920a17c4c2d7ef557bcf4c9863d23dd3cf2e
anniekellner <annie.kellner@colostate.edu>
2024-08-19 12:25:17 -0600

    Successfully wrote code to transform monthSum df's so that columns = month and rows = Avg values

--- Code/Plots/Excel_Plots.R
+++ Code/Plots/Excel_Plots.R
@@ -5,6 +5,7 @@
 # 8-18-2024
 # written by Annie Kellner, annie.kellner@colostate.edu
 
+
 # ------------  Create Directory for Plots  ----------------------- #
 
 xl_plots_dir <- paste(plots_dir,"Excel_Plots", sep = "/") # Change this folder name -- ASK TREVOR
@@ -13,9 +14,27 @@ if (!dir.exists(xl_plots_dir)){
   dir.create(xl_plots_dir)}
 
 
-# Historical Average Temperature
+# ----------  Loop to reformat dataframes for use with ggplot2 --------------------  #
+
 
 df <- monthSum$results_baseline[1:12,]
+df <- df %>%  select(-month)
+
+col_names <- colnames(df)
+
+dfT <- data.table::transpose(df) 
+
+rownames(dfT) <- col_names
+colnames(dfT) <- month.abb
+
+
+ 
+
+df2 <- pivot_longer(
+  data = df, 
+  cols = starts_with("Avg"), 
+  names_to = "Month",
+  names_prefix = )
 
 # Pivot plot so that columns names are month.abb and rows are Avg_
 # See this example (or others)
@@ -30,7 +49,7 @@ billboard %>%
     values_drop_na = TRUE
   )
 
-df2 <- pivot_longer(data = df, cols = starts_with("Avg"), names_to = month.abb)
+
 
 hist_Tmean_ave <- ggplot(data = df, aes(x = month, y = Avg_TMeanF)) +
   geom_bar(aes(color = "#CBC598", fill = "#CBC598"))
