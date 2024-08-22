01f58b1b0df0a1482a89162d64b8d745b7d0d1a2
Annie Kellner <annie.kellner@colostate.edu>
2024-08-22 15:31:04 -0600

    Aesthetics approximate Trevor's Excel plots. Check with him to make sure

--- Code/Plots/Excel_Plots.R
+++ Code/Plots/Excel_Plots.R
@@ -18,29 +18,32 @@ if (!dir.exists(xl_plots_dir)){
 
 
 df <- monthSum$results_baseline[1:12,]
+
 df <- df %>%  
   select(-month) %>%
-  mutate(Month = month.abb) %>% glimpse()
+  mutate(Month = month.abb) 
            
-           
-           month(df$month, label = TRUE, abbr = TRUE, locale = Sys.getlocale("LC_TIME")))
+
+# Plots
+
+p <- ggplot(df) +
+  geom_col(aes(x = factor(Month, levels = c(month.abb)), y = Avg_TMeanF), color="#CBC598", fill="#CBC598", width = 0.7) +
+  xlab(paste0("\n", "Month")) +
+  ylab(paste0("Average Temperature (\u00B0F)", "\n")) +
+  labs(title = "Historical Average Temperature") +
+  theme(element_text(family = "serif", size = 9, hjust = 0.5),
+        plot.title = element_text(family = "serif", hjust = 0.5, size = 9),
+        axis.title = element_text(family = "serif", hjust = 0.5, size = 9),
+        panel.background = element_blank(), 
+        panel.grid.major.y = element_line(color = "grey", linetype = "solid", linewidth = 0.5),
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(margin = margin(t = -40, r = 0, b = 0, l = 0)))
+          
+  
+  
 
 
-ggplot(df) +
-geom_col(aes(x = factor(Month, levels = c(month.abb)), y = Avg_TMeanF), color="#CBC598", fill="#CBC598") 
 
 
-#Historical: #CBC598
-
-#col_names <- colnames(df)
-
-#dfT <- data.table::transpose(df) 
-
-#rownames(df) <- col_names
-#colnames(df) <- month.abb
-
-
-#rownames(dfT) <- month.abb
-#colnames(dfT) <- col_names
 
 #### USE CODE FROM CLIMOGRAPHS SCRIPT
