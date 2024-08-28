337a24dc16aee3ac47305f0e3c6b68a9efd49f39
Annie Kellner <annie.kellner@colostate.edu>
2024-08-27 18:46:10 -0600

    think this should work. testing.

--- Code/Plots/Excel_Plots.R
+++ Code/Plots/Excel_Plots.R
@@ -36,32 +36,44 @@ for(i in 1:length(monthSum)){
 ## Prep data
 
 df <- monthSum[[1]]
-df <- df[,3:5] # select only relevant columns for plots
-df <- select(df, 3,1,2) # reorder columns to match 
+
+df <- df %>%
+  select(Month, Avg_TMeanF, Avg_TMaxF, Avg_TMinF)
+
 titles <- c("Historical Average Temperature", "Historical Average Maximum Temperature", "Historical Average Minimum Temperature")
 plots <- c("Hist Avg_TMeanF", "Hist Avg_TMaxF", "Hist Avg_TMinF")
-
+y_cols <- c("Avg_TMeanF", "Avg_TMaxF", "Avg_TMinF")
 
 ## Create Plots
 
-for(i in 1:length(titles)){
+plot_list <- list() # Need to create list in order to run plots through a loop
 
-ggplot(df) +
-  geom_col(aes(x = factor(Month, levels = c(month.abb)), y = colnames(df)[i]), color="#CBC598", fill="#CBC598", width = 0.7) +
+for(y_col in y_cols){
+
+p <- ggplot(df) +
+  geom_col(aes(x = factor(Month, levels = c(month.abb)), y = !!sym(y_col)), color="#CBC598", fill="#CBC598", width = 0.7) + # code for the y column points ggplot2 to the correct column within the dataframe
   xlab(paste0("\n", "Month")) +
-  ylab(paste0("Average Temperature (\u00B0F)", "\n")) +
-  labs(title = titles[i]) +
-  theme(element_text(family = "serif", hjust = 0.5),
+  ylab(paste0("Average Temperature (\u00B0F)", "\n"))
+  
+ plot_list[[y_col]] <- p
+}
+  
+for(i in 1:length(plot_list)){  
+  p = plot_list[[i]] +
+    labs(title = titles[i]) +
+    scale_y_continuous(limits = c(0,100), n.breaks = 11) +
+    theme(element_text(family = "serif", hjust = 0.5),
         plot.title = element_text(family = "serif", hjust = 0.5, size = 12),
         axis.title = element_text(family = "serif", hjust = 0.5, size = 10),
         panel.background = element_blank(), 
         panel.grid.major.y = element_line(color = "grey", linetype = 1, linewidth = 0.25), # linetype = 1 is a solid line. Not sure why it appears dashed, but won't be very noticeable in print
         axis.ticks = element_blank(),
         axis.text.x = element_text(margin = margin(t = 0.1, r = 0, b = 0, l = 0), size = 8),
-        axis.text.y = element_text(size = 8)) # See whether - values put the text above the axis. If so, adjust
+        axis.text.y = element_text(size = 8)) 
+    
           
  ggsave(filename = paste0(titles[i],'.png'), 
-        plot = plots[i],
+        plot = p,
         path = './Results/Test-Excel_Plots',
         width = 5.5,
         height = 3,
