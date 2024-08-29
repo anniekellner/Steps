e4796834768961a4c653ed9dfc4d6ab444fed459
Annie Kellner <annie.kellner@colostate.edu>
2024-08-29 14:10:56 -0600

    cleaned up extraneous code. Possible final version for historical plots, but need to test

--- Code/Plots/Hist_Bar_Charts.R
+++ Code/Plots/Hist_Bar_Charts.R
@@ -5,45 +5,19 @@
 # 8-18-2024
 # written by Annie Kellner, annie.kellner@colostate.edu
 
+# ------------- Prep data ----------------------------- #
 
-# ------------  Create Directory for Plots  ----------------------- #
-
-xl_plots_dir <- paste(plots_dir,"Bar_Charts", sep = "/") 
-
-if (!dir.exists(xl_plots_dir)){
-  dir.create(xl_plots_dir)}
-
-
-# ------------  Create function for adding month (e.g., "Jan") to dataframe --------------  #
-
-add_month <- function(df){
-  df = monthSum[[i]][1:12,]
-  df = select(df, -month)
-  df = mutate(df, Month = month.abb)
-}
-
-
-# ----------  PLOTS - HISTORICAL --------------------  #  
-
-## Prep data
-
-# Loop to reformat dataframes for use with ggplot2 
-
-for(i in 1:length(monthSum)){ ### This might not be necessary considering only the historical df is used - but used later for diffHist
-  monthSum[[i]] = add_month(monthSum[[i]])
-}
-
-         
 df <- monthSum[[1]] # Historical data
 
-df <- df %>%
-  select(Month, Avg_TMeanF, Avg_TMaxF, Avg_TMinF)
+df <- df 
+df <- add_month(df) %>% ## Make sure this works
+df <- select(df, Month, Avg_TMeanF, Avg_TMaxF, Avg_TMinF)
 
 titles <- c("Historical Average Temperature", "Historical Average Maximum Temperature", "Historical Average Minimum Temperature")
 plots <- c("Hist Avg_TMeanF", "Hist Avg_TMaxF", "Hist Avg_TMinF")
 y_cols <- c("Avg_TMeanF", "Avg_TMaxF", "Avg_TMinF")
 
-## Create Plots
+# ------------- Create Plots  --------------------------  #
 
 plot_list <- list() # Need to create list in order to run plots through a loop
 
@@ -80,54 +54,3 @@ for(i in 1:length(plot_list)){
         dpi = 300) 
 }
  
-##  ------  PLOTS - CHANGE FROM HISTORICAL  ------------ ##
-
-for(i in 1:length(diffHist)){ 
-  diffHist[[i]] = add_month(diffHist[[i]])
-}
-
-# Create new df's to compare changes within-scenario
-
-
-# Scenario 1 (e.g., SSP2-4.5)
-
-s1f1 <- diffHist[[1]][1:12,] # eliminate summary rows
-s1f2 <- diffHist[[2]][1:12,]
-
-s1f1 <- s1f1 %>%
-  rename_with(~paste0(., "_F1"), starts_with("Avg")) %>%
-  rename_with(~paste0(., "_F1"), starts_with("Abs")) 
-
-s1f2 <- s1f2 %>%
-  rename_with(~paste0(., "_F2"), starts_with("Avg")) %>%
-  rename_with(~paste0(., "_F2"), starts_with("Abs"))
-
-S1 <- full_join(s1f1, s1f2, by = "Month") 
-
-
-# Scenario 2 (e.g., SSP2-8.5)
-
-s2f1 <- diffHist[[3]][1:12,] # eliminate summary rows
-s2f2 <- diffHist[[4]][1:12,]
-
-s2f1 <- s2f1 %>%
-  rename_with(~paste0(., "_F1"), starts_with("Avg")) %>%
-  rename_with(~paste0(., "_F1"), starts_with("Abs")) 
-
-s2f2 <- s2f2 %>%
-  rename_with(~paste0(., "_F2"), starts_with("Avg")) %>%
-  rename_with(~paste0(., "_F2"), starts_with("Abs"))
-
-S2 <- full_join(s2f1, s2f2, by = "Month") 
-
-# ----------- 
-
-
-
-
-
-
-
-
-
-
