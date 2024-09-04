210ef47e0496806bc7fdd069832a0c77e08f72a4
Annie Kellner <annie.kellner@colostate.edu>
2024-09-04 13:43:07 -0600

    loop appears to work for S2

--- Code/Plots/Comparison_Bar_Charts.R
+++ Code/Plots/Comparison_Bar_Charts.R
@@ -118,15 +118,63 @@ ggsave(filename = paste0(fileNames45[i],'.png'),
 }
 
 
-#   ------------------------------------    ##
+##   --------- PLOTS FOR SCENARIO 2  ---------------------------    ##
 
-tempPlots85 <- c("Change in TAve 8.5", "Change in TMax 8.5", "Change in TMin 8.5")
+plotList_S2 <- list()
+
+fileNames85 <- c("Change in TAve 8.5",
+                 "Change in TMax 8.5",
+                 "Change in TMin 8.5")
 
 tempTitles85 <- c(
   paste(scenario_plotNames[3],"Change in Average Temperature", sep = " "),
   paste(scenario_plotNames[3],"Change in Average Maximum Temperature", sep = " "),
-  paste(scenario_plotNames[3], "Change in Average Minimum Temperature", sep = " "),
+  paste(scenario_plotNames[3], "Change in Average Minimum Temperature", sep = " ")
 )
+
+## Loop ##
+
+for(y_col in y_cols){
+  p = ggplot(S1, aes(x = factor(Month, levels = c(month.abb)), y = !!sym(y_col), fill = Future)) + 
+    geom_bar(stat = "identity", position = "dodge", color = "black", width = 0.7) + # code for the y column points ggplot2 to the correct column within the df
+    xlab(paste0("\n", "Month")) +
+    ylab(paste0("Change in temperature (\u00B0F)", "\n"))
+  
+  plotList_S2[[y_col]] <- p
+  
+}
+
+for(i in 1:length(plotList_S2)){  
+  p = plotList_S2[[i]] +
+    labs(title = tempTitles85[i]) +
+    scale_y_continuous(limits = c(0,10), n.breaks = 6) +
+    scale_fill_manual(values = custom_fill_temp, labels = custom_labels) +
+    theme(element_text(family = "serif", hjust = 0.5),
+          plot.title = element_text(family = "serif", hjust = 0.5, size = 12),
+          axis.title = element_text(family = "serif", hjust = 0.5, size = 10),
+          panel.background = element_blank(), 
+          panel.grid.major.y = element_line(color = "grey", linetype = 1, linewidth = 0.25), # linetype = 1 is a solid line. Not sure why it appears dashed, but won't be very noticeable in print
+          axis.ticks = element_blank(),
+          axis.text.x = element_text(margin = margin(t = 0.1, r = 0, b = 0, l = 0), size = 8),
+          axis.text.y = element_text(size = 8),
+          legend.position = "bottom",
+          legend.title = element_blank(), 
+          legend.box.margin = margin(t = 0, r = 50, b = 0, l = 50),
+          legend.key.spacing.x = unit(0.5, "in")) + 
+    guides(fill = guide_legend(byrow = TRUE))
+  
+  ggsave(filename = paste0(fileNames85[i],'.png'), 
+         plot = p,
+         path = './Results/Test-Excel_Plots',
+         width = 5.5,
+         height = 3,
+         units = "in",
+         dpi = 300) 
+  
+}
+
+
+
 
 pS1 <- ggplot(S1) +
   geom_col(aes(x = factor(Month, levels = c(month.abb)), y = !!sym(y_col)), color="#CBC598", fill="#CBC598", width = 0.7) )
