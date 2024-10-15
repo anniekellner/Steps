9ef3e075a39ff340ec3ca593e5c772b60d0ad076
Annie Kellner <annie.kellner@colostate.edu>
2024-10-14 23:01:00 -0600

    Added titles to formatting script (and removed from plot creation scripts).
    Need to test to see how it looks. Attempting to center titles

--- Code/Tables_and_Plots/Format_Bar_Charts.R
+++ Code/Tables_and_Plots/Format_Bar_Charts.R
@@ -4,6 +4,21 @@
 
 # Use package {patchwork}
 
+# Set plot elements
+
+histTitles <- c("Historical Average Temperature", 
+            "Historical Average Maximum Temperature", 
+            "Historical Average Minimum Temperature")
+
+tempTitles <- c(
+  "Projected Change in Average Temperature",
+  "Projected Change in Average Maximum Temperature",
+  "Projected Change in Average Minimum Temperature"
+)
+
+prcpTitle <- "Projected Change in Average Precipitation"
+
+
 # Change in Average Temperature
 
 Hist_AvgTemp <- hist_plots[[1]]
@@ -15,7 +30,7 @@ rcp85AvgTemp <- temp_plots_S2[[1]]
 theme_border <- theme_gray() + 
   theme(plot.background = element_rect(fill = NA, 
                                        color = "#899691", 
-                                       size = 1))
+                                       linewidth = 1))
 
 # Combine futures into single plot
 futures <- ({rcp45AvgTemp / rcp85AvgTemp}) +
@@ -26,6 +41,12 @@ futures <- ({rcp45AvgTemp / rcp85AvgTemp}) +
   
 # Use wrap_elements() function to limit border to futures plots
 
+wrap_elements(panel = grid.text(histTitles[1],
+                                just = "centre",
+                                gp = gpar(fontsize = 12,
+                                          fontfamily = "calibri",
+                                          fontface = "bold"))) /
+                                
 wrap_elements(panel = Hist_AvgTemp) / 
   wrap_elements(panel = grid.text(tempTitles[1],
                                   just = "centre",
@@ -33,7 +54,7 @@ wrap_elements(panel = Hist_AvgTemp) /
                                             fontfamily = "serif",
                                             fontface = "bold"))) /
   wrap_elements(panel = futures) + 
-  plot_layout(heights = c(2,0.1,2))
+  plot_layout(heights = c(0.1,2,0.1,2))
 
 
 
