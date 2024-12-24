17e703ae1e7289211b66d2aa4dcb5dc333f5313e
anniekellner <annie.kellner@colostate.edu>
2024-12-23 17:09:02 -0700

    unify labeling of 'month' between AllDays and monthSum df's (for NOAA data)

--- Code/NOAA/NOAA_Climographs.R
+++ Code/NOAA/NOAA_Climographs.R
@@ -14,14 +14,39 @@
 
 ##############################################################################
 
-# Take revelant columns from monthSum
+##    ----  PREP DATA   -------   ##
 
-df_msX <- noaa_monthSum[[1]]
 
-from_MS <- df_msX %>%
-  select(Avg_month, Avg_PPT_in, Avg_TMaxF, Avg_TMinF)
+# Unify labeling 
 
-# Calculate max and min temps from AllDays_hist
+# Create function for adding month label (e.g., "Jan") to AllDays_hist dataframe
+
+add_month <- function(df){
+  df = df %>%
+    mutate(month = month(df$date, 
+                         label = TRUE, 
+                         abbr = TRUE, 
+                         locale = Sys.getlocale("LC_TIME")))
+}
+
+
+## Add month label to AllDays_hist dataframe
+
+for(i in 1:length(AllDays_hist)){
+  AllDays_hist[[i]] = add_month(AllDays_hist[[i]])
+  AllDays_hist[[i]]$month = as.character(AllDays_hist[[i]]$month)
+}
+
+
+## Add month label to noaa_monthSum dataframe
+
+df_msX$Avg_month <- month.abb[df_msX$Avg_month]
+
+
+
+##    ---   COMBINE DATAFRAMES  --    ##
+
+## Extract max and min values from AllDays_hist 
 
 adX <- AllDays_hist[[1]]
 
@@ -32,6 +57,18 @@ Abs_TMinF <- adX %>%
 Abs_TMaxF <- adX %>%
   group_by(month) %>%
   summarize(AbsTMaxF = max(TMaxF, na.rm = TRUE))
+
+
+## Select relevant columns from noaa_monthSum
+
+df_msX <- noaa_monthSum[[1]]
+
+from_MS <- df_msX %>%
+  select(Avg_month, Avg_PPT_in, Avg_TMaxF, Avg_TMinF)
+
+
+##  Combine dataframes
+
 
 
 
