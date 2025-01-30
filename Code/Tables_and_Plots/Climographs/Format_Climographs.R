7e7ad0fd249d2d54cd0fe5443d410bb3d374efe4
anniekellner <annie.kellner@colostate.edu>
2025-01-30 12:51:48 -0700

    Added ggsave() to save comparative plots. Everything works great.

--- Code/Tables_and_Plots/Climographs/Format_Climographs.R
+++ Code/Tables_and_Plots/Climographs/Format_Climographs.R
@@ -18,11 +18,61 @@ ssp45_nearTerm <- (climPlots[[1]] + plot_spacer() + climPlots[[2]]) + # plot_spa
 filename_ssp45_nearTerm <- paste(shp,"climographs_hist_ssp45_nearTerm.png", sep = "_") 
 
 ggsave(filename = filename_ssp45_nearTerm,
-       plot = ssp45,
+       plot = ssp45_nearTerm,
        path = path_to_climographs,
        width = 8.5,
        height = 5.5,
        units = "in",
        dpi = 330)
 
-ssp45_farTerm <- 
+ssp45_farTerm <- (climPlots[[1]] + plot_spacer() + climPlots[[3]]) + # plot_spacer() adds a dummy plot to make the two closer together
+  plot_layout(guides = "collect", widths = c(4,-1.1,4)) &
+  theme(legend.position = "bottom",
+        legend.direction = "vertical")
+
+filename_ssp45_farTerm <- paste(shp,"climographs_hist_ssp45_farTerm.png", sep = "_") 
+
+ggsave(filename = filename_ssp45_farTerm,
+       plot = ssp45_farTerm,
+       path = path_to_climographs,
+       width = 8.5,
+       height = 5.5,
+       units = "in",
+       dpi = 330)
+
+## SSP-8.5 
+
+# Near Term
+
+ssp85_nearTerm <- (climPlots[[1]] + plot_spacer() + climPlots[[4]]) + # plot_spacer() adds a dummy plot to make the two closer together
+  plot_layout(guides = "collect", widths = c(4,-1.1,4)) &
+  theme(legend.position = "bottom",
+        legend.direction = "vertical")
+
+filename_ssp85_nearTerm <- paste(shp,"climographs_hist_ssp85_nearTerm.png", sep = "_") 
+
+ggsave(filename = filename_ssp85_nearTerm,
+       plot = ssp85_nearTerm,
+       path = path_to_climographs,
+       width = 8.5,
+       height = 5.5,
+       units = "in",
+       dpi = 330)
+
+# Far Term
+
+ssp85_farTerm <- (climPlots[[1]] + plot_spacer() + climPlots[[5]]) + # plot_spacer() adds a dummy plot to make the two closer together
+  plot_layout(guides = "collect", widths = c(4,-1.1,4)) &
+  theme(legend.position = "bottom",
+        legend.direction = "vertical")
+
+filename_ssp85_farTerm <- paste(shp,"climographs_hist_ssp85_farTerm.png", sep = "_") 
+
+ggsave(filename = filename_ssp85_farTerm,
+       plot = ssp85_farTerm,
+       path = path_to_climographs,
+       width = 8.5,
+       height = 5.5,
+       units = "in",
+       dpi = 330)
+
