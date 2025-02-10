977afc0204469ca8420d8e1d29f636485e60c75d
anniekellner <annie.kellner@colostate.edu>
2025-02-10 06:11:47 -0700

    removed MACA code; monthSum df for future use is now monthSumDF; dataframe for csv files is monthSum.
    Also updated code to reflect new summarise() syntax, added humidity vars to csv. Waiting on Andy for VPD instructions

--- Code/monthSum.R
+++ Code/monthSum.R
@@ -2,6 +2,7 @@
 ###         MONTHLY SUMMARY OF VARIABLES                                    ###
 ###############################################################################
 
+# amended by Annie Kellner (annie.kellner@colostate.edu) 2-6-2025
 
 # This script produces the following outputs:
   # monthSum.csv for traditional use with written reports
@@ -13,14 +14,11 @@
 
 
 
-# amended by Annie Kellner (annie.kellner@colostate.edu) 2-6-2025
+###   BEGIN SCRIPT  ###
 
 
-monthSum <- list()
+monthSumDF <- list()
 
-MACA = if_else(str_detect(model, "MACA"), # this code is not yet functional, but does not disrupt the process
-               TRUE,
-               FALSE)
 
 for(i in 1:length(AllDays)){
   df = AllDays[[i]]
@@ -31,42 +29,48 @@ for(i in 1:length(AllDays)){
     dplyr::select(-c(lat, lon))
   
   yearAvg = df %>%
-    dplyr::select(!c('date','PPT_in', 'PPT_mm', 'GDDF')) %>% # Exclude variables for which the result is not simply a monthly average
+    dplyr::select(!c('date','PPT_in', 'PPT_mm', 'GDDF')) %>% # exclude variables for which the result is not simply a monthly average
     dplyr::select(!(contains("days"))) %>%
     group_by(year, month) %>%
-    summarise(across(where(is.numeric), mean))
+    summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
+    ungroup()
   
   Abs_TminF = df %>%
     select(date, year, month, TMinF) %>%
     group_by(year, month) %>%
-    summarise(Abs_TminF = min(TMinF))
+    summarise(Abs_TminF = min(TMinF)) %>%
+    ungroup()
   
   sum_ppt = df %>%
     select(date, year, month, 'PPT_in', 'PPT_mm') %>%
     group_by(year, month) %>%
-    summarise(across(contains('PPT'), sum))
+    summarise(across(contains('PPT'), ~ sum(.x, na.rm = TRUE))) %>%
+    ungroup()
   
   sum_days = df %>%
     select(date, year, month, contains('days')) %>%
     group_by(year, month) %>%
-    summarise(across(contains('days'), sum))
+    summarise(across(contains('days'), ~ sum(.x, na.rm = TRUE))) %>%
+    ungroup()
   
   sum_GDDF = df %>%
     select(date, year, month, GDDF) %>%
     group_by(year, month) %>%
-    summarise(GDDF = sum(GDDF))
+    summarise(GDDF = sum(GDDF, na.rm = TRUE)) %>%
+    ungroup()
   
   all = yearAvg %>%
-    full_join(sum_days) %>%
-    full_join(sum_ppt) %>%
-    full_join(Abs_TminF) %>%
-    full_join(sum_GDDF) %>%
+    left_join(sum_days) %>%
+    left_join(sum_ppt) %>%
+    left_join(Abs_TminF) %>%
+    left_join(sum_GDDF) %>%
     ungroup()
   
   monthAvg = all %>%
     dplyr::select(!year) %>%
     group_by(month) %>%
-    summarise(across(everything(), mean)) %>%
+    summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+    round(digits = 1) %>%
     setNames(paste0('Avg_', names(.))) %>%
     rename(Abs_TminF = Avg_Abs_TminF) %>%
     select(Avg_month, # put in order on MonthSum csv
@@ -76,55 +80,44 @@ for(i in 1:length(AllDays)){
            Avg_TMinF, 
            Avg_TMeanF, 
            Abs_TminF, 
-           any_of("Avg_hurs"),
-           any_of("Avg_sfcWind"),
            Avg_GDDF,
            Avg_hotdays,
            Avg_colddays,
            Avg_wetdays,
            Avg_drydays,
-           Avg_ftdays
+           Avg_ftdays,
+           contains("hu") # for the time being, huss = 0 because the values are so small. Waiting on Andy for VPD info
     )
   
-  # As above, MACA-related code is a placeholder for future integration
-  
-  macaCols = if(MACA == "TRUE") {getMACAcols(all)} else {NULL} 
-  
-  monthAvg = if(MACA == "TRUE") {bind_cols(monthAvg, macaCols)} else {monthAvg} 
-  
-  # End of MACA code
-  
-  monthSum[[i]] = monthAvg # new df for export and use with DiffHist
-  names(monthSum)[i] = names(AllDays[i])
+  monthSumDF[[i]] = monthAvg # new df for export and use with DiffHist
+  names(monthSumDF)[i] = names(AllDays[i])
 }
 
 # Add summary rows (YrAverage and YrTotals) and save for table construction
 
 Avs_and_Totals <- list() # saving for summary table
+monthSum <- list() # with final two summary rows (for csv creation)
 
-for(i in 1:length(monthSum)){
-  YrAverage = colMeans(monthSum[[i]][,2:ncol(monthSum[[i]])]) # converts columns to characters 
-  YrTotals = colSums(monthSum[[i]][,2:ncol(monthSum[[i]])])
-  csv = bind_rows(monthSum[[i]], YrAverage, YrTotals)
+for(i in 1:length(monthSumDF)){
+  YrAverage = colMeans(monthSumDF[[i]][,2:ncol(monthSumDF[[i]])]) # converts columns to characters 
+  YrTotals = colSums(monthSumDF[[i]][,2:ncol(monthSumDF[[i]])])
+  csv = bind_rows(monthSumDF[[i]], YrAverage, YrTotals)
   
   # NA's 
   
-  NAs = csv %>% # YrAverage
+  NAs = csv %>% # NA's for YrAverage
     filter(row_number() == 13) %>% # Because there will always be 12 months irrespective of model
     mutate(across(.cols = contains("PPT"), ~na_if(.,.))) %>%
     mutate(across(.cols = contains("days"), ~na_if(.,.))) %>%
     mutate(across(.cols = contains("Abs"), ~na_if(.,.))) %>%
     mutate(across(.cols = contains("GDDF"), ~na_if(.,.))) 
-  
-  NAs_Totals = csv %>% # YrTotals
+
+  NAs_Totals = csv %>% # NA's for YrTotals
     filter(row_number() == 14) %>%
     mutate(across(.cols = contains("Avg_T"), ~na_if(.,.))) %>%
     mutate(across(.cols = contains("Abs"), ~na_if(.,.))) %>%
-    mutate(across(.cols = any_of("Avg_hurs"), ~na_if(.,.))) %>%
-    mutate(across(.cols = any_of("Avg_sfcWind"), ~na_if(.,.))) 
-  
-  NAs_Totals = if(MACA == "TRUE") {mutate(across(.cols = .cols %in% colnames(macaCols), ~na_if(.,.)))
-  } else {NAs_Totals}
+    mutate(across(.cols = any_of("Avg_sfcWind"), ~na_if(.,.))) %>%
+    mutate(across(.cols = contains("hu"), ~na_if(.,.)))
   
   csv = csv %>%
     slice(1:(n()-2)) %>% # remove summary rows
