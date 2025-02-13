e1198a1e1cf4e2fc43246933e8ad24c541476367
anniekellner <annie.kellner@colostate.edu>
2025-02-12 19:37:55 -0700

    ClimographsDF script seems to work

--- Code/ClimographsDF.R
+++ Code/ClimographsDF.R
@@ -9,7 +9,7 @@
 
 # Inputs: 
     # AllDays dataframe (Tibble)
-    # monthAvg dataframe (Tibble)
+    # monthSumDF dataframe (Tibble)
 
 # Output:
     # clim = list of dataframes with Precip sum (in), Avg TMaxF, Avg TMinF, upper 90% quantile, lower 10% quantile
@@ -20,29 +20,14 @@
 ## -------  Create matching 'month' columns between AllDays and monthSum  ----  ##
 
 
-for(i in 1:length(monthSum)){
-  monthSum[[i]] = monthSum[[i]][1:12,]} # remove last two rows 
-  
-
-for(i in 1:length(monthSum)){
-  monthSum[[i]]$month = as.numeric(monthSum[[i]]$month)
-}
-
-
-for(i in 1:length(monthSum)){
-  monthSum[[i]]$month = month.abb[monthSum[[i]]$month]
-}
-
-
-# Add 'month' column to AllDays dataframe
-
+# AllDays
 
 for(i in 1:length(AllDays)){ # Convert date column to ymd (year-month-day in {lubridate})
   df = AllDays[[i]]
   df$date = ymd(df$date)
   AllDays[[i]] = df
 }
-  
+
 for(i in 1:length(AllDays)){
   df = AllDays[[i]]
   df = df %>%
@@ -54,6 +39,20 @@ for(i in 1:length(AllDays)){
   AllDays[[i]]$month = month.abb[AllDays[[i]]$month]
 }
 
+
+# monthSum
+
+for(i in 1:length(monthSumDF)){
+  monthSumDF[[i]]$Avg_month = month.abb[monthSumDF[[i]]$Avg_month]
+}
+
+for(i in 1:length(monthSumDF)){
+  df = monthSumDF[[i]]
+  
+  df = df %>%
+    rename(month = Avg_month)
+    monthSumDF[[i]] = df
+}
 ## -----  Calculate quantiles:  90% TMaxF, 10% TMinF  ------------  ##
 
 
@@ -65,35 +64,38 @@ for(i in 1:length(AllDays)){
   
   high90 = df %>%
     group_by(month) %>%
-    summarize(high90 = quantile(TMaxF, probs = 0.90, na.rm = TRUE))
+    summarize(high90 = quantile(TMaxF, probs = 0.90, na.rm = TRUE)) %>%
+    ungroup()
   
   low10 = df %>%
     group_by(month) %>%
-    summarize(low10 = quantile(TMinF, probs = 0.10, na.rm = TRUE))
+    summarize(low10 = quantile(TMinF, probs = 0.10, na.rm = TRUE)) %>%
+    ungroup()
   
-  high90low10 = full_join(high90, low10)
+  high90low10 = left_join(high90, low10)
   
   quantiles[[i]] = high90low10 
   
 }
 
-### SELECT RELEVANT COLUMNS FROM MONTHSUM AND JOIN WITH QUANTILES  ###
+### SELECT RELEVANT COLUMNS FROM monthSumDF AND JOIN WITH QUANTILES  ###
 
 
 clim <- list()
 
 
-for(i in 1:length(monthSum)){
+for(i in 1:length(monthSumDF)){
   
-  climDF = select(monthSum[[i]], month, Avg_PPT_in, Avg_TMaxF, Avg_TMinF)
+  climDF = select(monthSumDF[[i]], month, Avg_PPT_in, Avg_TMaxF, Avg_TMinF)
   
   climDF = climDF %>%
     rename(PPT_in = Avg_PPT_in) %>%
     rename(TMaxF = Avg_TMaxF) %>%
     rename(TMinF = Avg_TMinF) 
+
   
   # Combine with AbsMinMax data
   
-  clim[[i]] = full_join(climDF, quantiles[[i]])
+  clim[[i]] = left_join(climDF, quantiles[[i]])
   
 }
