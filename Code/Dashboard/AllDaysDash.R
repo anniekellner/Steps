c78da7cdb8baded2fbcb0b4bf6ee60986fb1dc4d
anniekellner <annie.kellner@colostate.edu>
2025-02-26 08:11:11 -0700

    Creates 30 year averages based on daily data. Appears to work; waiting for TE approval

--- Code/Dashboard/AllDaysDash.R
+++ Code/Dashboard/AllDaysDash.R
@@ -24,6 +24,10 @@ AllDaysDash <- list() # required for monthSumDash
 
 for(i in 1:length(avdf)){
   csv = avdf[[i]]
+  
+  csv$date = ymd(csv$date) # change date to lubridate type
+  csv$Year = Year(csv$date) # add Year for summed variables
+  
   csv = csv %>%
     mutate(PPT_mm = case_when( # if raster units are "kg-m-2 -1", convert to mm (otherwise keep as is)
       str_detect(units(rx), "kg") ~ prcp*86400,
@@ -44,11 +48,11 @@ for(i in 1:length(avdf)){
     mutate(TMeanF = (TMaxF + TMinF)/2) %>%
     mutate(TmeanC = (TMaxC + TMinC)/2) %>%
     mutate(GDDF = RasterGDD(TMinF, TMaxF, 50, 86)) %>% # All hard-coded values are from the original CEMML script
-    mutate(hotdays = Rasterhotdays(TMaxC, hottemp = 32.2)) %>% # Hard-coded values may be changed at user discretion
-    mutate(colddays = Rastercolddays(TMinC, coldtemp = 0)) %>%
-    mutate(wetdays = Rasterwetdays(PPT_mm, wetprecip = 50.8)) %>%
-    mutate(drydays = Rasterdrydays(PPT_mm, dryprecip = 2.54)) %>%
-    mutate(ftdays = RasterFTdays(TMaxC, TMinC, freezethresh = -2.2, thawthresh = 1.2)) %>%
+    mutate(HOTDAYS = Rasterhotdays(TMaxC, hottemp = 32.2)) %>% # Hard-coded values may be changed at user discretion
+    mutate(COLDDAYS = Rastercolddays(TMinC, coldtemp = 0)) %>%
+    mutate(WETDAYS = Rasterwetdays(PPT_mm, wetprecip = 50.8)) %>%
+    mutate(DRYDAYS = Rasterdrydays(PPT_mm, dryprecip = 2.54)) %>%
+    mutate(FTDAYS = RasterFTdays(TMaxC, TMinC, freezethresh = -2.2, thawthresh = 1.2)) %>%
     mutate(VHOTDAYS = fnVHOTDAYS(TMaxC, hottemp = 37.8)) %>%
     mutate(EXHOTDAYS = fnEXHOTDAYS(TMaxC, hottemp = 37.8)) %>%
     mutate(HELLDAYS = fnHELLDAYS(TMaxC, hottemp = 40.6)) %>%
@@ -56,15 +60,37 @@ for(i in 1:length(avdf)){
     mutate(FRFRDAYS = fnFRFRDAYS(TMinC, coldtemp = 0)) %>%
     mutate(VWETDAYS = fnVWETDAYS(PPT_mm, wetprecip = 101.6))
   
+  csv <- select(csv,  # remove Celsius values; add new variables for Viewer (2-26-2025)
+               Year,
+               PPT_in, 
+               TMaxF, 
+               TMinF, 
+               TMeanF, 
+               GDDF, 
+               HOTDAYS, 
+               COLDDAYS, 
+               WETDAYS, 
+               DRYDAYS, 
+               FTDAYS,
+               VHOTDAYS,
+               EXHOTDAYS,
+               HELLDAYS,
+               WARMNIGHTS,
+               FRFRDAYS,
+               VWETDAYS)
+  
+  
   AllDaysDash[[i]] = csv # new df for use with MonthSum
   names(AllDaysDash)[i] = names(avdf[i])
   
 } # end creation of AllDaysDash dataframe
 
 
-# ----    CALCULATE 30-YEAR AVERAGES  ------  #
+# ----    CALCULATE 30-Year AVERAGES  ------  #
 
-alldays30 <- for(i in 1:length(AllDaysDash)){
+alldays30 <- list()
+
+for(i in 1:length(AllDaysDash)){
   
   df = AllDaysDash[[i]] 
   
@@ -81,8 +107,7 @@ alldays30 <- for(i in 1:length(AllDaysDash)){
     ungroup()
   
   YearAvg = df %>%
-    dplyr::select(!c('date', 'Year', 'PPT_in', 'GDDF')) %>% # exclude variables for which the result should be summed before averaging
-    dplyr::select(!(contains("days"))) %>%
+    dplyr::select(!c('Year', 'PPT_in', 'GDDF')) %>% # exclude variables for which the result should be summed before averaging
     select(!contains("DAYS")) %>%
     select(!contains("NIGHTS")) %>% 
     summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
@@ -100,13 +125,6 @@ alldays30 <- for(i in 1:length(AllDaysDash)){
   
   Pctl10_Prcp_in = sum_ppt %>%
     summarize(Pctl10_Prcp_in = quantile(PPT_in, probs = 0.10, na.rm = TRUE)) %>%
-    ungroup()
-  
-  
-  sum_days = df %>%
-    select(Year, contains('days')) %>%
-    group_by(Year) %>%
-    summarise(across(contains('days'), ~ sum(.x, na.rm = TRUE))) %>%
     ungroup()
   
   sum_DAYS = df %>%
@@ -127,7 +145,7 @@ alldays30 <- for(i in 1:length(AllDaysDash)){
     summarise(GDDF = sum(GDDF, na.rm = TRUE)) %>%
     ungroup()
   
-  sums = sum_days %>% # Averages by Year (e.g., 1981, 1982...2010)
+  sums = sum_DAYS %>% # Averages by Year (e.g., 1981, 1982...2010)
     left_join(sum_ppt) %>%
     left_join(sum_GDDF) %>%
     left_join(sum_DAYS) %>%
@@ -143,12 +161,58 @@ alldays30 <- for(i in 1:length(AllDaysDash)){
   
   all = bind_cols("ScenID" = ScenID, "PERIOD" = Period, Pctl90_Prcp_in, sumAvg, Pctl10_Prcp_in, Pctl90_TmaxF, Pctl10_TminF, YearAvg)
   
-  all = all %>%
+  all = all %>% # round
     mutate(across(Pctl90_Prcp_in:TMeanF,  ~ round(., digits = 1)))
+  
+  all = all %>% 
+    mutate(SCENARIO = case_when(
+      first(df$Year) == 1985 ~ "ModelHist",
+      first(df$Year) == 2021 & i == 2 ~ "SSP2-4.5",
+      first(df$Year) == 2051 & i == 3 ~ "SSP2-4.5",
+      first(df$Year) == 2021 & i == 4 ~ "SSP5-8.5",
+      first(df$Year) == 2051 & i == 5 ~ "SSP5-8.5") ) %>%
+    mutate(SITENAME = official_name) %>%
+    rename(Avg_Prcp_in = PPT_in) %>%
+    rename(Avg_TmaxF = TMaxF) %>%
+    rename(Avg_TmeanF = TMeanF) %>%
+    rename(Avg_TminF = TMinF) 
+
+  all = select(all,
+               SITENAME,
+               SCENARIO,
+               ScenID,
+               PERIOD,
+               Pctl90_Prcp_in,
+               Avg_Prcp_in,
+               Pctl10_Prcp_in,
+               Pctl90_TmaxF,
+               Avg_TmaxF,
+               Avg_TmeanF,
+               Avg_TminF,
+               Pctl10_TminF,
+               HOTDAYS,
+               VHOTDAYS,
+               EXHOTDAYS,
+               HELLDAYS,
+               WARMNIGHTS,
+               COLDDAYS,
+               FRFRDAYS,
+               FTDAYS,
+               GDDF,
+               DRYDAYS,
+               WETDAYS,
+               VWETDAYS)
   
   alldays30[[i]] = all
   
 }
+
+rm(df)
+
+# Combine noaa with AllDays
+
+avg30 <- bind_rows(list(noaa30, alldays30[[1]], alldays30[[2]], alldays30[[3]], alldays30[[4]], alldays30[[5]]))
+
 
 # --  SAVE SPREADSHEETS  --  #
 
@@ -157,5 +221,4 @@ dash_dir <- paste(results_folder,"Dashboard", sep = "/")
 if (!dir.exists(dash_dir)){
   dir.create(dash_dir)}
 
-write.csv(MonthlySeries, file = paste(dash_dir,"MonthlySeries.csv", sep = "/"))
-write.csv(avg30, file = paste(dash_dir, "30yr_Averages.csv", sep = "/"))
+write.csv(avg30, file = paste(dash_dir, "30yr_Averages.csv", sep = "/"), row.names = FALSE)
