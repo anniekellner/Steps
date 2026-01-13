62b2e09ed279003bd8eefc8a8cf6d3cb91d24d9a
anniekellner <annie.kellner@colostate.edu>
2026-01-13 01:57:53 -0700

    adjusted code so that max precip values > 2 can be accommodated, and also follow the y-axis scale (which increases in increments of 0.4)

--- Code/Tables_and_Plots/Bar_Charts/Compare_Bar_Charts.R
+++ Code/Tables_and_Plots/Bar_Charts/Compare_Bar_Charts.R
@@ -100,6 +100,7 @@ min_pr_value <- if_else(min_prcpS1 < min_prcpS2, min_prcpS1, min_prcpS2)
 # Assign plot limits
 
 prcp_upper_limit <- case_when(
+  max_pr_value > 2 ~ ceiling(max_pr_value / 0.4) * 0.4,
   max_pr_value <= 2 ~ 2,
   max_pr_value > 2 & max_pr_value <= 2.4 ~ 2.4,
   max_pr_value > 2.4 & max_pr_value <= 2.8 ~ 2.8,
